811e15ba978e7750f47337c25d293395a15e9e31
Kelly Barry <kleemuir@gmail.com>
2022-09-21 15:46:02 -0600

    Save new variable

--- friends.js
+++ friends.js
@@ -1,8 +1,14 @@
 var bestFriend = ["Adina"]
+var bestFriend1 = ["Nikki"]
 
 if (bestFriend === "Adina") {
   console.log("Hello Adina!");
 }
+
+else if (bestFriend1 === "Nikki") {
+  console.log("Hello Nikki!");
+}
+
 else {
   console.log("Hello loser!")
 }
